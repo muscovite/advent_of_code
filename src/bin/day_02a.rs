3c21b29812b86082b7a09f542a54a4b9948d06d4
Yvonne <yvonnechen.91@hotmail.com>
2020-12-03 16:30:34 -0800

    Day 2

---
+++ src/bin/day_02a.rs
@@ -0,0 +1,58 @@
+use lazy_static::lazy_static;
+use regex::Regex;
+
+lazy_static! {
+    static ref RE: Regex =
+        Regex::new(r"(?P<min>\d+)-(?P<max>\d+) (?P<rule>[a-z]): (?P<password>[a-z]+)").unwrap();
+}
+
+fn solve(input: &str) -> usize {
+    fn is_valid(line: &str) -> bool {
+        let captures = RE.captures(line).unwrap();
+        let min = captures.name("min").unwrap().as_str().parse().unwrap();
+        let max = captures.name("max").unwrap().as_str().parse().unwrap();
+        let rule = captures
+            .name("rule")
+            .unwrap()
+            .as_str()
+            .chars()
+            .next()
+            .unwrap();
+        let password = captures.name("password").unwrap().as_str().chars();
+        let mut count = 0;
+        for c in password {
+            if c == rule {
+                count += 1;
+            }
+        }
+
+        if count >= min && count <= max {
+            true
+        } else {
+            false
+        }
+    }
+
+    input.trim().lines().filter(|line| is_valid(line)).count()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case1() {
+        let input = r"1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc";
+        assert_eq!(solve(input), 2);
+    }
+
+    #[test]
+    fn case2() {
+        let input = r"1-3 z: abcde";
+        assert_eq!(solve(input), 0);
+    }
+}
+
+advent_2020::read_main!();
---
+++ src/bin/day_02b.rs
@@ -0,0 +1,59 @@
+use lazy_static::lazy_static;
+use regex::Regex;
+
+lazy_static! {
+    static ref RE: Regex =
+        Regex::new(r"(?P<min>\d+)-(?P<max>\d+) (?P<rule>[a-z]): (?P<password>[a-z]+)").unwrap();
+}
+
+fn solve(input: &str) -> usize {
+    fn is_valid(line: &str) -> bool {
+        let captures = RE.captures(line).unwrap();
+        let min: usize = captures.name("min").unwrap().as_str().parse().unwrap();
+        let max: usize = captures.name("max").unwrap().as_str().parse().unwrap();
+        let rule = captures
+            .name("rule")
+            .unwrap()
+            .as_str()
+            .chars()
+            .next()
+            .unwrap();
+        let password: Vec<char> = captures
+            .name("password")
+            .unwrap()
+            .as_str()
+            .chars()
+            .collect();
+
+        if password[min - 1] == rule && password[max - 1] == rule {
+            false
+        } else if password[min - 1] == rule || password[max - 1] == rule {
+            true
+        } else {
+            false
+        }
+    }
+
+    input.trim().lines().filter(|line| is_valid(line)).count()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case1() {
+        let input = r"1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc";
+        assert_eq!(solve(input), 1);
+    }
+
+    #[test]
+    fn case2() {
+        let input = r"1-3 z: zbcde";
+        assert_eq!(solve(input), 1);
+    }
+}
+
+advent_2020::read_main!();
